76a5048958b26557d3e29379623cdd8e6692e487
Jeongwon Park <j2982477j@gmail.com>
2024-01-29 16:40:36 +0900

    첨부파일 있을 경우 포스트 상세 페이지에 다운롣 버튼 출력하기

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import os
 # Create your models here.
 
 
@@ -19,3 +19,9 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)  #파일명 반환
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]  #파일 확장자명 반환
