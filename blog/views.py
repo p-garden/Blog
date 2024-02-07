ddcc2d715662a7af9481b8f491ff20db20e9a2a1
Jeongwon Park <j2982477j@gmail.com>
2024-02-07 17:05:50 +0900

    태그 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 
 class PostList(ListView):
@@ -12,6 +12,16 @@ class PostList(ListView):
         context= super(PostList, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count']=Post.objects.filter(category=None).count()
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
 def category_page(request,slug):
@@ -33,15 +43,21 @@ def category_page(request,slug):
             'category': category,
         }
     )
-class PostDetail(DetailView):
-    model = Post
 
-    def get_context_data(self, **kwargs):
-        context = super(PostDetail, self).get_context_data()
-        context['categories'] = Category.objects.all()
-        context['no_category_post_count'] = Post.objects.filter(category=None).count()
-        return context
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
 
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'tag': tag,
+        }
+    )
 # Create your views here.
 """def index(request):
     posts = Post.objects.all().order_by('-pk') #최신순으로 보기
