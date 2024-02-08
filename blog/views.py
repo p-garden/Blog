c96a637465f9badc26173a21460ca5aa31a2f4b7
Jeongwon Park <j2982477j@gmail.com>
2024-02-08 17:39:17 +0900

    포스트 수정 기능 추가하기

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post, Category, Tag
-
+from django.core.exceptions import PermissionDenied
 
 class PostList(ListView):
     model = Post
@@ -76,6 +76,17 @@ class PostCreate(LoginRequiredMixin,UserPassesTestMixin,CreateView):
             return redirect('/blog/') #로그인 안된 경우는 원래 블로그 페이지로 돌려 보내기
 
 
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+
+    template_name = 'blog/post_update_form.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
 
 # Create your views here.
 """def index(request):
