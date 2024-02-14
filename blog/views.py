7dadc1f81ebd030f510a888be52b84d7824f153d
Jeongwon Park <j2982477j@gmail.com>
2024-02-14 17:21:17 +0900

    포스트를 수정할 때 텍스트로 태그 지정하기

--- blog/views.py
+++ blog/views.py
@@ -96,15 +96,44 @@ class PostCreate(LoginRequiredMixin,UserPassesTestMixin,CreateView):
 
 class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
     template_name = 'blog/post_update_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostUpdate, self).get_context_data()
+        if self.object.tags.exists():
+            tags_str_list = list()
+            for t in self.object.tags.all():
+                tags_str_list.append(t.name)
+            context['tags_str_default']= '; '.join(tags_str_list)
+        return context
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated and request.user == self.get_object().author:
             return super(PostUpdate, self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
+
+    def form_valid(self, form):
+        response = super(PostUpdate, self).form_valid(form)
+        self.object.tags.clear()
+
+        tags_str = self.request.POST.get('tags_str')
+        if tags_str:               #태그가 존재한다면  처리해줘야함
+            tags_str = tags_str.strip() #태그 각자 분리후 , ; 모두 구분자로 처리
+
+            tags_str  =tags_str.replace(',', ';')
+            tags_list = tags_str.split(';')
+
+            for t in tags_list:
+                t = t.strip()  #각 태그의 앞뒤 공백 제거
+                tag, is_tag_created = Tag.objects.get_or_create(name=t) #tag에 tag모델의 인스턴스 is~에 인스턴스 생성되었는지 부울값 저장
+                if is_tag_created:  #새로 생성하였다면 slug값 생성
+                    tag.slug = slugify(t, allow_unicode=True) #한글도 허용
+                    tag.save()
+                self.object.tags.add(tag) #새로운 포스트에 태그 추가
+        return response
 
 # Create your views here.
 """def index(request):
