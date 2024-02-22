2a7756a6bc9badca5afa465e696680e36551c093
Jeongwon Park <j2982477j@gmail.com>
2024-02-22 21:31:09 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -69,3 +69,9 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/2052/191de7d326bcd31b/svg/{self.author.email}'
