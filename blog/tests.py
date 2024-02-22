a759048e9171c2e7c86a281c5f897d90ffdc7db8
Jeongwon Park <j2982477j@gmail.com>
2024-02-22 17:02:36 +0900

    댓글 수정 기능 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -342,6 +342,53 @@ class TestView(TestCase):
         self.assertIn('PG', new_comment_div.text)
         self.assertIn('PG의 댓글입니다.', new_comment_div.text)
 
+    def test_comment_update(self):
+        comment_by_SC=Comment.objects.create(
+            post=self.post_001,
+            author = self.user_SC,
+            content='SC의 댓글입니다.'
+        )
+        response = self.client.get(self.post_001.get_absolute_url())
+        self.assertEqual(response.status_code,200)
+        soup=BeautifulSoup(response.content, 'html.parser')
 
+        comment_area = soup.find('div', id='comment-area')
+        self.assertFalse(comment_area.find('a', id='comment-1-update-btn'))
+        self.assertFalse(comment_area.find('a', id='comment-2-update-btn'))
 
+        #로그인한 상태
+        self.client.login(username='PG', password='wjddnjs0418')
+        response=self.client.get(self.post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        comment_area = soup.find('div', id='comment-area')
+        self.assertFalse(comment_area.find('a', id='comment-2-update-btn'))
+        comment_001_update_btn = comment_area.find('a', id='comment-1-update-btn')
+        self.assertIn('edit', comment_001_update_btn.text)
+        self.assertEqual(comment_001_update_btn.attrs['href'], '/blog/update_comment/1/')
+        
+        #댓글 수정 파트
+        response = self.client.get('/blog/update_comment/1/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        self.assertEqual('Edit Comment - Blog', soup.title.text)
+        update_comment_form = soup.find('form', id='comment-form')
+        content_textarea = update_comment_form.find('textarea', id='id_content')
+        self.assertIn(self.comment_001.content, content_textarea.text)
+
+        response = self.client.post(
+            f'/blog/update_comment/{self.comment_001.pk}/',
+            {
+                'content': "PG의 댓글을 수정합니다.",
+            },
+            follow=True
+        )
+
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        comment_001_div = soup.find('div', id='comment-1')
+        self.assertIn('PG의 댓글을 수정합니다.', comment_001_div.text)
+        self.assertIn('Updated: ', comment_001_div.text)
 
